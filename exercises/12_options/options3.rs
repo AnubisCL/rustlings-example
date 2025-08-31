9de8f5bb0cf0e9a6f25cf495641267d731d17ac1
chailei <anubiscl@163.com>
2025-08-31 09:46:35 +0800

    feat: 12_options 3 ref / &

--- exercises/12_options/options3.rs
+++ exercises/12_options/options3.rs
@@ -8,8 +8,10 @@ fn main() {
     let optional_point = Some(Point { x: 100, y: 200 });
 
     // TODO: Fix the compiler error by adding something to this match statement.
+    // match &optional_point { // 匹配整个 Option 的引用
     match optional_point {
-        Some(p) => println!("Co-ordinates are {},{}", p.x, p.y),
+        // 这是模式匹配语法，表示"匹配一个引用并将其绑定到 p
+        Some(ref p) => println!("Co-ordinates are {},{}", p.x, p.y),
         _ => panic!("No match!"),
     }
 
